62ed55226cf8407af108b8fe991f1bf219d7f9b8
wqh <337096961@qq.com>
2018-06-19 13:06:27 +0800

    xiugai

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 #include "mpi.h"
 
 int array[4];
-int PASSNUM = 0;
 int PASSTEMP = 0;
+int PASSSUM ;
 
 boolean calculate(int x, int CalculateTime) {
     int i, j, temp, min, max;
@@ -25,7 +25,6 @@ boolean calculate(int x, int CalculateTime) {
     max = array[3] * 1000 + array[2] * 100 + array[1] * 10 + array[0];
     x = max - min;
     if (x == 6174 && CalculateTime <= 7) {
-        ++PASSNUM;
         ++PASSTEMP;
         return TRUE;
     }
@@ -40,7 +39,7 @@ void run(int fromWhere, int endWhere, int rank) {
         calculate(i, 1);
     }
     printf("第%d个进程成功的个数：%d\n", rank, PASSTEMP);
-    PASSTEMP = 0;
+
 }
 
 int main() {
@@ -51,31 +50,49 @@ int main() {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     int counnt = 9999 - 1000 + 1;
     int eachCalucate = counnt / size;
-    printf("size:%d\n",size);
 
     switch (size) {
         case 1:
+            if (rank==0)
             run(1000, 9999, rank);
             break;
         case 2:
-            run(1000, 1000 + eachCalucate, rank);
-            run(1000 + eachCalucate + 1, 9999, rank++);
+            if (rank==0){
+                run(1000, 1000 + eachCalucate, rank);
+            } else if (rank==1)
+            run(1000 + eachCalucate + 1, 9999, rank);
             break;
         case 3:
-            run(1000, 1000 + eachCalucate, rank);
-            run(1000 + eachCalucate + 1, 1000 + eachCalucate * 2, rank++);
-            run(1000 + eachCalucate * 2 +1, 9999, rank++);
+            if (rank==0){
+                run(1000, 1000 + eachCalucate, rank);
+            } else if (rank==1){
+                run(1000 + eachCalucate + 1, 1000 + eachCalucate * 2, rank);
+            } else if (rank==2)
+            run(1000 + eachCalucate * 2 +1, 9999, rank);
             break;
         case 4:
-            run(1000, 1000 + eachCalucate, rank);
-            run(1000 + eachCalucate + 1, 1000 + eachCalucate * 2, rank++);
-            run(1000 + eachCalucate * 2 +1, 1000 + eachCalucate * 3, rank++);
-            run(1000 + eachCalucate * 3 +1, 9999, rank++);
+            if (rank==0){
+                run(1000, 1000 + eachCalucate, rank);
+            } else if (rank==1){
+                run(1000 + eachCalucate + 1, 1000 + eachCalucate * 2, rank);
+            } else if (rank==2){
+                run(1000 + eachCalucate * 2 +1, 1000 + eachCalucate * 3, rank);
+            } else if (rank==3){
+                run(1000 + eachCalucate * 3 +1, 9999, rank);
+            }
+
+
+
+
             break;
         default:
             break;
+
     }
-    printf("总共成功的次数是%d\n",PASSNUM);
+    MPI_Reduce(&PASSTEMP, &PASSSUM, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD);
+    if (rank==0)
+    printf("总共成功的个数%d\n",PASSSUM);
     MPI_Finalize();
     return 0;
 }
